28d38c019a15c92eb45f8fe32d8fc411878a871e
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2017-11-12 13:16:41 +0900

    rubocop指摘

--- Gemfile
+++ Gemfile
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
-source "https://rubygems.org"
 
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+source 'https://rubygems.org'
 
-gem 'appium_lib'
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
+
 gem 'appium_console'
+gem 'appium_lib'
 gem 'rspec'
 gem 'selenium-webdriver'
--- spec/startup_spec.rb
+++ spec/startup_spec.rb
@@ -2,15 +2,19 @@ require 'appium_lib'
 
 describe 'setup' do
   before do
-    caps   = { caps:       { platformName: 'Android',
-                             deviceName: 'Nexus 7',
-                             appActivity: '.StartActivity',
-                             appPackage: 'com.twitter.android',
-                             unicodeKeyboard:  'true',
-    },
-
-               appium_lib: { sauce_username: nil,
-                             sauce_access_key: nil } }
+    caps = {
+      caps: {
+        platformName: 'Android',
+        deviceName: 'Nexus 7',
+        appActivity: '.StartActivity',
+        appPackage: 'com.twitter.android',
+        unicodeKeyboard:  'true'
+      },
+      appium_lib: {
+        sauce_username: nil,
+        sauce_access_key: nil
+      }
+    }
     driver = Appium::Driver.new(caps, true)
     Appium.promote_appium_methods self.class
     driver.start_driver.manage.timeouts.implicit_wait = 20 # seconds
@@ -31,7 +35,8 @@ describe 'setup' do
 
   it '中断できること' do
     find_element(:id, 'com.twitter.android:id/composer_write').click
-    find_element(:id, 'com.twitter.android:id/tweet_text').send_keys 'プログラミング合宿最高!! #nshgrb'
+    find_element(:id, 'com.twitter.android:id/tweet_text')
+      .send_keys 'プログラミング合宿最高!! #nshgrb'
     driver.save_screenshot('aaa.png')
     sleep 5
     find_element(:id, 'com.twitter.android:id/up_button').click
