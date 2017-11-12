7c21876de245edf5f778b4f6eeb2791d640ccd78
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2017-11-12 13:00:03 +0900

    検索のテスト

--- spec/startup_spec.rb
+++ spec/startup_spec.rb
@@ -14,6 +14,7 @@ describe 'setup' do
     driver = Appium::Driver.new(caps, true)
     Appium.promote_appium_methods self.class
     driver.start_driver.manage.timeouts.implicit_wait = 20 # seconds
+    scroll_to('OK').click
   end
 
   after do
@@ -21,7 +22,6 @@ describe 'setup' do
   end
 
   it '通知を表示' do
-    scroll_to('OK').click
     find_element(:id, 'com.twitter.android:id/notifications').click
     scroll_to('達人').click
     sleep 5
@@ -30,12 +30,20 @@ describe 'setup' do
   end
 
   it '中断できること' do
-    scroll_to('OK').click
     find_element(:id, 'com.twitter.android:id/composer_write').click
     find_element(:id, 'com.twitter.android:id/tweet_text').send_keys 'プログラミング合宿最高!! #nshgrb'
     driver.save_screenshot('aaa.png')
     sleep 5
     find_element(:id, 'com.twitter.android:id/up_button').click
     scroll_to('削除').click
+  end
+
+  it '検索ができること' do
+    find_element(:id, 'com.twitter.android:id/moments').click
+    find_element(:id, 'com.twitter.android:id/query_view').click
+    find_element(:id, 'com.twitter.android:id/query').send_keys "#nshgrb\n"
+    scroll_to('いい感じ').click
+    driver.save_screenshot('bbb.png')
+    sleep 5
   end
 end
